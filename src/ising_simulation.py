293fd2510a6da2afdfa432001cce6e7e1d778a3e
Maximilian Negedly <maximilian.negedly@univie.ac.at>
2024-03-20 17:48:26 +0100

    Added analysis for tanh(beta B) behaviour at J=0 and different temperatures

--- src/ising_simulation.py
+++ src/ising_simulation.py
@@ -2,12 +2,50 @@ from ising import generate_configuration, propagate, Trajectory, filter_trajecto
 from multiprocessing import Process
 import numpy as np
 
-def simulate_temperatures(N, J, B, temperatures):
-    for temperature in temperatures:
-        configuration = generate_configuration(N=N, random=False)
-        filename = generate_trajectory_name(N=N, J=J, B=B, temperature=temperature, folder="ising_analysis_traj")
-        propagate(configuration=configuration, n_timestep=1000000, n_output=1000, J=J, B=B, temperature=temperature, filename=filename)
 
+def simulate(N, J, B, temperature):
+    Ns = [N] if not isinstance(N, list) and not isinstance(N, np.ndarray) else N
+    Js = [J] if not isinstance(J, list) and not isinstance(J, np.ndarray) else J
+    Bs = [B] if not isinstance(B, list) and not isinstance(B, np.ndarray) else B
+    temperatures = [temperature] if not isinstance(temperature, list) and not isinstance(temperature, np.ndarray) else temperature
+
+    for temperature in temperatures:
+        for N in Ns:
+            for J in Js:
+                for B in Bs:
+                    configuration = generate_configuration(N=N, random=False)
+                    filename = generate_trajectory_name(N=N, J=J, B=B, temperature=temperature, folder="ising_analysis_traj")
+                    propagate(configuration=configuration, n_timestep=100000, n_output=100, J=J, B=B, temperature=temperature, filename=filename)
+
+# Different B simulations at different temperatures
+if __name__ == "__main__":
+    temperatures = [0.3]#[0.001, 0.3, 1.0, 2.0]
+
+    J = 0
+    Bs = np.round(np.arange(-2.0, 2.0, 0.01), 2)
+    N = 10
+
+    processes = 4
+
+    # Divide Bs to simulate in blocks of mostly equal size (+/- 1) according to number of processes
+    n_Bs = len(Bs)
+    Bs_per_block = n_Bs//processes
+    residuals = n_Bs%processes
+    Bs = [list(Bs[i*Bs_per_block:(i+1)*Bs_per_block]) + ([Bs[-residuals+i]] if i < residuals and residuals > 0 else []) for i in range(processes)]
+
+
+    procs = []
+    for i in range(processes):
+        proc = Process(target=simulate, args=(N, J, Bs[i], temperatures))
+        procs.append(proc)
+        proc.start()
+        print(f"Started process {i+1} of {processes} with {len(Bs[i])} Bs")
+
+    for proc in procs:
+        proc.join()
+
+# Different temperature simulations
+"""
 if __name__ == "__main__":
     temperatures = np.round(np.arange(0.01, 3.51, 0.01), 2)
 
@@ -17,7 +55,7 @@ if __name__ == "__main__":
 
     processes = 8
 
-    # Divide temperatures to simulate in blocks of mostly equal size (+/- 1) according to numbr of processes
+    # Divide temperatures to simulate in blocks of mostly equal size (+/- 1) according to number of processes
     n_temps = len(temperatures)
     temps_per_block = n_temps//processes
     residuals = n_temps%processes
@@ -26,10 +64,11 @@ if __name__ == "__main__":
 
     procs = []
     for i in range(processes):
-        proc = Process(target=simulate_temperatures, args=(N, J, B, temps[i]))
+        proc = Process(target=simulate, args=(N, J, B, temps[i]))
         procs.append(proc)
         proc.start()
         print(f"Started process {i+1} of {processes} with {len(temps[i])} temperatures")
 
     for proc in procs:
         proc.join()
+"""
