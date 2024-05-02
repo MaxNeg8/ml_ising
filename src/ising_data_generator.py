0aa8b28a47b1f51a904c9e9460dced5130c39fe0
Maximilian Negedly <maximilian.negedly@univie.ac.at>
2024-05-02 18:21:22 +0200

    Added train/test parameter to data generator, added train/test data for N=16 and N=25

--- src/ising_data_generator.py
+++ src/ising_data_generator.py
@@ -2,7 +2,7 @@ from ising import generate_configuration, propagate, save_configurations_bin
 import time
 import numpy as np
 
-def simulate(N: int, J : float, B : float, temperature : float, n_samples : int = 1):
+def simulate(N: int, J : float, B : float, temperature : float, training : bool, n_samples : int = 1):
     """
     Simulate Ising model for given parameters to sample n_samples uncorrelated configurations
     from the respective configuration space. All parameters can either be given as single
@@ -22,6 +22,9 @@ def simulate(N: int, J : float, B : float, temperature : float, n_samples : int 
     temperature': float
         Simulation temperature
 
+    training : bool
+        Whether or not this data set is to be regarded as a training set (influences the file name)
+
     n_samples : int
         Number of samples to generate from each configuration space
     """
@@ -30,20 +33,20 @@ def simulate(N: int, J : float, B : float, temperature : float, n_samples : int 
     Js = [J] if not isinstance(J, list) and not isinstance(J, np.ndarray) else J
     Bs = [B] if not isinstance(B, list) and not isinstance(B, np.ndarray) else B
     temperatures = [temperature] if not isinstance(temperature, list) and not isinstance(temperature, np.ndarray) else temperature
-
+    train_test = "train" if training else "test"
     for B in Bs:
         for N in Ns:
             for J in Js:
                 filename = "ising_ml_data/" + f"N_{N}_J_{J}_B_{B}_"
                 labels = np.array([temperatures[i//n_samples] for i in range(len(temperatures)*n_samples)])
-                np.savetxt(filename + "labels_train.csv", labels)
+                np.savetxt(filename + f"labels_{train_test}.csv", labels)
                 configurations = np.empty((n_samples*len(temperatures), N, N), dtype=int)
                 for i,temperature in enumerate(temperatures):
                     for sample in range(n_samples):
                         configuration = generate_configuration(N=N, random=True)
                         propagate(configuration=configuration, n_timestep=300, n_output=0, J=J, B=B, temperature=temperature, filename=None, algorithm="wolff")
                         configurations[i*n_samples + sample] = configuration
-                save_configurations_bin(filename + "data_train", configurations, allow_overwrite=True)
+                save_configurations_bin(filename + f"data_{train_test}", configurations, allow_overwrite=True)
 
 
 def main():
@@ -51,9 +54,9 @@ def main():
 
     J = 1
     B = 0
-    N = 10
+    N = 25
 
-    simulate(N, J, B, temperatures, 5)
+    simulate(N, J, B, temperatures, training=True, n_samples=5)
 
 if __name__ == "__main__":
     main()
